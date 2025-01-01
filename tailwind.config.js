1fa2618dc84771fba385a10134365ef30caa90bb
TranDanh1122 <trandanh14042000@gmail.com>
2025-01-01 11:27:25 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        red: 'hsl(0, 100%, 74%)',
+        green: 'hsl(154, 59%, 51%)',
+        blue: 'hsl(248, 32%, 49%)',
+        darkBlue: 'hsl(249, 10%, 26%)',
+        grayishBlue: 'hsl(246, 25%, 77%)'
+      },
+      screens: {
+        mb: { min: "0", max: "767px" }
+      }
+    },
+  },
+  plugins: [],
+}
+
